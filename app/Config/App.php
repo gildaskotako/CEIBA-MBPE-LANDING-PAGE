f116aa20c3aa43abd6988ee961437872adffb846
kouassi kotako Gildas <32975045+gildaskotako@users.noreply.github.com>
2024-12-13 11:24:31 +0000

    LAst Updateq

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8088/';
+    public string $baseURL = 'https://archivagegouv-ci.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
